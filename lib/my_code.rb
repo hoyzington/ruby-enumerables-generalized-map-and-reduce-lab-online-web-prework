e36f6add4d7c69f84024f7645fc5802cd601a696
Scott Hoisington <hoyzington@gmail.com>
2019-11-18 20:03:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,12 +9,12 @@ def map(array)
 end
 
 
-def reduce(array, x = 0)
-result = x
+def reduce(a, sp = 0)
+result = sp
 i = 0
-while i < array.size
-  result = yield(result, array[i])
-  if yield(result, array[i]) == !true
+while i < a.size
+  result = yield(result, a[i])
+  if result == !true
     return false
   end
   i += 1
@@ -24,15 +24,3 @@ if result == 0
 end
 result
 end
-
-
-#def reduce(array)
-#  i = 0
-#  while i < array.size
-#    if array[i] == true
-#      return true
-#    end
-#    i += 1
-#  end
-#  return false
-#end
